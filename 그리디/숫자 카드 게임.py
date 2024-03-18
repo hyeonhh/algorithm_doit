4459e6d071f5444746d35667a02c64299b91bc92
hyeonhh <tjdgustjdan@gmail.com>
2024-03-18 16:53:45 +0900

    ✨ 그리기

---
+++ 그리디/숫자 카드 게임.py
@@ -0,0 +1,15 @@
+n,m = map(int,input().split())
+
+data =[]
+result = 0
+
+for i in range(n):
+  data = list(map(int,input().split()))
+  min_value = 0
+  for a in data:
+    min_value = min(min_value,a)
+  result = max(result,min_value)
+print(result)
+
+
+rmf
