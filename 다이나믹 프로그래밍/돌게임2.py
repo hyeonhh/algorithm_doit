786f4e8714229608d15f674fe918ab72b7e3cf7a
hyeonhh <tjdgustjdan@gmail.com>
2024-06-04 13:41:43 +0900

    DP : 돌문제 2 dp로 풀기

--- 다이나믹 프로그래밍/돌게임2.py
+++ 다이나믹 프로그래밍/돌게임2.py
@@ -4,24 +4,20 @@ input = sys.stdin.readline
 
 N = int(input())
 
-# dp = [0] * 
+dp = [-1] * 1001 
 
-# dp[0] = 0 
-# dp[1] = 0 # 0이면 상근 1이면 창영
+dp[1] = 1 # 0이면 상근 1이면 창영이가 이김 
+dp[2] = 0
+dp[3] = 1
 
 
-# # for i in range(2,N):
-# #   if dp[i-1] == 0:
-# #     dp[i] = 1
-# #   else :
-# #     dp[i] = 0
+for i in range(4,N+1):
+  if dp[i-1] or dp[i-3]:
+    dp[i] = 0
+  else :
+    dp[i] = 1
 
-if N % 2 ==0:
+if dp[N] == 0:
   print("SK")
 else:
   print("CY")
-
-# if dp[N-1] == 0:
-#   print("SK")
-# else:
-#   print("CY")
