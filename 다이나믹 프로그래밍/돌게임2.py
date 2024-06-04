16005e30ce3cf3b65d1e54530ee541a995cc6402
hyeonhh <tjdgustjdan@gmail.com>
2024-06-04 13:46:27 +0900

    dp : 돌문제 2 개선

--- 다이나믹 프로그래밍/돌게임2.py
+++ 다이나믹 프로그래밍/돌게임2.py
@@ -6,18 +6,15 @@ N = int(input())
 
 dp = [-1] * 1001 
 
-dp[1] = 1 # 0이면 상근 1이면 창영이가 이김 
-dp[2] = 0
-dp[3] = 1
+dp[1] = 'CY' # 0이면 상근 1이면 창영이가 이김 
+dp[2] = 'SK'
+dp[3] = 'CY'
 
 
 for i in range(4,N+1):
-  if dp[i-1] or dp[i-3]:
-    dp[i] = 0
+  if dp[i-1] == 'CY'or dp[i-3]=='CY':
+    dp[i] = 'SK'
   else :
-    dp[i] = 1
+    dp[i] = 'CY'
 
-if dp[N] == 0:
-  print("SK")
-else:
-  print("CY")
+print(dp[N])
