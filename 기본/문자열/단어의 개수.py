7add4aa8990f1c25e3a1aa2aaaae4f4637ef90b0
Sunghyeon <tjdgustjdan@gmail.com>
2024-10-07 11:25:27 +0900

    코드 수정

--- 기본/문자열/단어의 개수.py
+++ 기본/문자열/단어의 개수.py
@@ -1,11 +1,12 @@
-word = input()
-# print(len(word))
+word = input().split()
+print(len(word))
 
 # split(" ") 가 아니라 그냥 split()을 해주니까 맞았다.
-splited_word = word.split()
-for i in splited_word:
-    if i =='':
-        splited_word.remove(i)
-    else:
-        continue
-print(len(splited_word))
+# # 그런데 리스트 순회 도중 리스트 값을 제거하는 것은 위험하다고 한다!
+# splited_word = word.split()
+# for i in splited_word:
+#     if i =='':
+#         splited_word.remove(i)
+#     else:
+#         continue
+# print(len(splited_word))
